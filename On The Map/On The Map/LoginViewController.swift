562fbc65726bd7c9230a6e3901d1168bc74c879b
Bryan's Air <BryansAppleAir@BryBook.local>
2018-07-02 17:13:18 -0600

    feat add login view controller and
    nav bar/tab bar segues and
    
    tab bar images to tabs

---
+++ On The Map/On The Map/LoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginViewController.swift
+//  On The Map
+//
+//  Created by Bryan's Air on 7/2/18.
+//  Copyright © 2018 Bryborg Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    // MARK: Outlets and variables
+    @IBOutlet weak var loginButton: UIButton!
+    //@IBOutlet weak var signUpButton: UIButton!
+    
+    @IBAction func
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
