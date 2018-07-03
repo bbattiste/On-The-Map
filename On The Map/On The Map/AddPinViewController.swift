22affa13b6476dee5d40f81fcbf90c29e987081f
Bryan's Air <battiste1313@gmail.com>
2018-07-03 13:11:47 -0600

    feat add nav bar items to addPinView

--- On The Map/On The Map/AddPinViewController.swift
+++ On The Map/On The Map/AddPinViewController.swift
@@ -10,26 +10,15 @@ import UIKit
 
 class AddPinViewController: UIViewController {
 
+    //MARK: outlets and variables
+    @IBOutlet weak var navBar: UINavigationBar!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        navBar.isHidden = false
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
