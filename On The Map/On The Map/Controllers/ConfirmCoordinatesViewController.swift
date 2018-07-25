a624bccf7b25f4fd37ce02769cff77715208f449
Bryan's Air <battiste1313@gmail.com>
2018-07-25 13:18:09 -0600

    feat add confirmCoordinatesViewController and link with view in storyboard

---
+++ On The Map/On The Map/Controllers/ConfirmCoordinatesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ConfirmCoordinatesViewController.swift
+//  On The Map
+//
+//  Created by Bryan's Air on 7/25/18.
+//  Copyright © 2018 Bryborg Inc. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import MapKit
+
+class ConfirmCoordinatesViewController: UIViewController, MKMapViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    
+
+}
--- On The Map/On The Map/GCD.swift
+++ On The Map/On The Map/GCD.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
-//TODO: what does performUIUpdatesOnMain do?
+/* when you're performing some sort of asynchronous task, such as downloading photos, you don't want that task to happen on the main thread because that would bog down the UI or cause errors. On the other hand, you always want updates to the UI to happen on the main thread, and in fact sometimes iOS will throw and exception if it's not! So the function that you have posted will perform another function on the main thread. The function to perform on the main thread is passed in as a parameter. You would call the performUIUpdatesOnMain function, for example, in the completion handler of an asynchronous task */
+
 func performUIUpdatesOnMain(_ updates: @escaping () -> Void) {
     DispatchQueue.main.async {
         updates()
